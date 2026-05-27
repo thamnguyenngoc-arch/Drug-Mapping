cfd11e512b315d8464fae6503d721cde0efd5d4a
thamnguyenngoc-arch <tham.nguyenngoc@buymed.com>
2026-05-27 16:41:01 +0700

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,22 @@
+# =========================
+# config.py
+# =========================
+
+MATCH_CONFIG = {
+
+    "quick_threshold": 50,
+
+    "name_threshold": 40,
+
+    "weights": {
+        "name": 0.5,
+        "unit": 0.3,
+        "volume": 0.2
+    },
+
+    "bonus": {
+        "exact_unit": 5,
+        "exact_volume": 5
+    }
+
+}
